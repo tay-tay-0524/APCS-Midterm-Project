ea71dea5b5ee02e9cf1862f57f421be776ef4fd9
tay-tay-0524 <108963043+tay-tay-0524@users.noreply.github.com>
2025-01-15 11:14:53 -0500

    Create text-to-binary.py

---
+++ text-to-binary.py
@@ -0,0 +1,15 @@
+def change_text():
+    text = input("Enter any statement: ")
+    text2 = text.lower()
+
+    letters = {"a": "01100001", "b": "01100010", "c": "01100011", "d": "01100100", "e": "01100101", "f": "01100110", "g": "01100111", "h": "01101000", "i": "01101001", "j": "01101010", "k": "01101011", "l": "01101100", "m": "01101101", "n": "01101110", "o": "01101111", "p": "01110000", "q": "01110001", "r": "01110010", "s": "01110011", "t": "01110100", "u": "01110101", "v": "01110110", "w": "01110111", "x": "01111000", "y": "01111001", "z": "01111010"}
+
+    string = ""
+
+    for t in text:
+        for key,value in letters.items():
+            if t == key:
+                string = string + value
+
+    print(f"your word '{text}' in binary code is {string}")
+
